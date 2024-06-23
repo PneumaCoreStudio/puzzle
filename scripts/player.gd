01ba1825beeab9a4345005efb34655570633def7
hellotherekyle <humphrey.kyle@protonmail.com>
2024-06-23 17:05:25 -0300

    Updated stuff

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,17 @@
 class_name Player
 extends CharacterBody2D
 
+@onready var animation = $AnimationPlayer
+
 const SPEED = 300.0
+var strain : bool
+func _ready():
+	if strain == true:
+		animation.play(strain)
+
 
 func _physics_process(delta):
 	var direction = Input.get_vector("mv_left", "mv_right", "mv_up", "mv_down")
-	
 	velocity = lerp(velocity, direction * SPEED, 0.25)
 	velocity.normalized()
-
 	move_and_slide()
