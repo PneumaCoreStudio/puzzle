ad80f0f1c19434094f1827b03be45fdf840eba26
theNullinator <williams.justin.edward@proton.me>
2024-06-23 18:42:56 -0300

    .

--- scripts/block.gd
+++ scripts/block.gd
@@ -2,3 +2,13 @@ class_name Block
 extends RigidBody2D
 
 enum KEY_COLOR { RED, GREEN, BLUE }
+@export var key_color : KEY_COLOR
+
+func _ready():
+	match key_color:
+		KEY_COLOR.RED:
+			modulate = Color.DARK_RED
+		KEY_COLOR.GREEN:
+			modulate = Color.DARK_GREEN
+		KEY_COLOR.BLUE:
+			modulate = Color.DARK_BLUE
--- scripts/block_sensor.gd
+++ scripts/block_sensor.gd
@@ -1,11 +1,31 @@
 class_name BlockSensor
 extends Node2D
 
+enum SENSOR_COLOR { RED, GREEN, BLUE }
+@export var sensor_color : SENSOR_COLOR
+
 @onready var area_2d = $Area2D
 @onready var center = $Center
 
+var attached_block : Block = null
+
+func _ready():
+	match sensor_color:
+		SENSOR_COLOR.RED:
+			modulate = Color.RED
+		SENSOR_COLOR.GREEN:
+			modulate = Color.GREEN
+		SENSOR_COLOR.BLUE:
+			modulate = Color.BLUE
+			
 func _physics_process(delta):
-	var bodies = area_2d.get_overlapping_bodies()
-	for body in bodies:
-		if body is Block && body.global_position != center.global_position:
-			body.global_position = body.position.move_toward(center.global_position, 1.0)
+	if !attached_block:
+		var bodies = area_2d.get_overlapping_bodies()
+		for body in bodies:
+			if body is Block && body.key_color == sensor_color:
+				if body.global_position != center.global_position:
+					body.global_position = body.position.move_toward(center.global_position, 2.0)
+					
+				else:
+					body.modulate = Color.WHITE
+					attached_block = body
