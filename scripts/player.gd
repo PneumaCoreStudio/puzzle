612567ee1ca3c5f6ff7be0c80a5dda24febce3e9
theNullinator <williams.justin.edward@proton.me>
2024-06-23 16:21:13 -0300

    added block

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,7 @@ const SPEED = 300.0
 func _physics_process(delta):
 	var direction = Input.get_vector("mv_left", "mv_right", "mv_up", "mv_down")
 	
-	velocity = lerp(velocity, direction * SPEED, 0.5)
+	velocity = lerp(velocity, direction * SPEED, 0.25)
+	velocity.normalized()
 
 	move_and_slide()
