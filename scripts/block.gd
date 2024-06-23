87c673c39d2cdb02e2390e3f332d38b53b26fa7e
theNullinator <williams.justin.edward@proton.me>
2024-06-23 17:53:10 -0300

    updated block sensor

--- scripts/block.gd
+++ scripts/block.gd
@@ -1,2 +1,4 @@
 class_name Block
 extends RigidBody2D
+
+enum KEY_COLOR { RED, GREEN, BLUE }
--- scripts/block_sensor.gd
+++ scripts/block_sensor.gd
@@ -2,12 +2,10 @@ class_name BlockSensor
 extends Node2D
 
 @onready var area_2d = $Area2D
-@onready var collision_shape_2d : CollisionShape2D = $Area2D/CollisionShape2D
+@onready var center = $Center
 
-func _ready():
-	area_2d.body_entered.connect(_on_body_entered)
-	
-func _on_body_entered(body : Node2D):
-	if body is Block:
-		print("TURE")
-		body.position = lerp(body.position, collision_shape_2d.shape.get_rect().get_center(), 0.125)
+func _physics_process(delta):
+	var bodies = area_2d.get_overlapping_bodies()
+	for body in bodies:
+		if body is Block && body.global_position != center.global_position:
+			body.global_position = body.position.move_toward(center.global_position, 1.0)
