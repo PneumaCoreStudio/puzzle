be23f7c43b31c3fa03d6ac5b14e072954f2777cb
theNullinator <williams.justin.edward@proton.me>
2024-06-23 15:52:31 -0300

    added player

---
+++ scripts/player.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+func _physics_process(delta):
+	var direction = Input.get_vector("mv_left", "mv_right", "mv_up", "mv_down")
+	
+	velocity = lerp(velocity, direction * SPEED, 0.5)
+
+	move_and_slide()
