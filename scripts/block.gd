e9ca24a46e968efe459fb42c11c6d1d523438fc8
theNullinator <williams.justin.edward@proton.me>
2024-06-23 16:38:25 -0300

    added block sensor

---
+++ scripts/block.gd
@@ -0,0 +1,2 @@
+class_name Block
+extends RigidBody2D
---
+++ scripts/block_sensor.gd
@@ -0,0 +1,12 @@
+class_name BlockSensor
+extends Node2D
+
+@onready var area_2d = $Area2D
+@onready var collision_shape_2d : CollisionShape2D = $Area2D/CollisionShape2D
+
+func _ready():
+	area_2d.body_entered.connect(_on_body_entered)
+	
+func _on_body_entered(body : Node2D):
+	if body is Block:
+		body.position = lerp(body.position, collision_shape_2d.shape.get_rect().get_center(), 0.125)
