3c59b55f5d4c796dca65ad23befb7a3eb1dece79
theNullinator <williams.justin.edward@proton.me>
2024-06-28 22:50:47 -0300

    fixed tween with door.

--- scripts/door.gd
+++ scripts/door.gd
@@ -7,26 +7,27 @@ var doorTween : Tween
 @onready var sprite_l = $door_L/Sprite2D
 @onready var sprite_r = $door_R/Sprite2D
 var open_time : float = .1
-var open_width : int = 30
+var open_width : int = 15
 
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		if !doorTween or !doorTween.is_valid():
 			doorTween = create_tween()
-
+			doorTween.set_parallel()
+			
 		if !open && doorTween.is_valid():
 			#doorTween.tween_property(sprite_l, "modulate", Color("GREEN"), .02)
 			#doorTween.parallel().tween_property(sprite_r, "modulate", Color("GREEN"), .02)
-			doorTween.parallel().tween_property(door_l, "global_position:x", global_position.x - open_width, open_time)
-			doorTween.parallel().tween_property(door_r, "global_position:x", global_position.x + open_width, open_time)
+			doorTween.tween_property(door_l, "global_position:x", global_position.x - open_width, open_time).as_relative()
+			doorTween.tween_property(door_r, "global_position:x", global_position.x + open_width, open_time).as_relative()
 			await doorTween.finished
 			open = true
 		elif open && doorTween.is_valid():
 			#doorTween.tween_property(sprite_l, "modulate", Color("RED"), .02)
 			#doorTween.parallel().tween_property(sprite_r, "modulate", Color("RED"), .02)
-			doorTween.tween_property(door_l, "global_position:x", global_position.x + open_width, open_time)
-			doorTween.parallel().tween_property(door_r, "global_position:x", global_position.x - open_width, open_time)
+			doorTween.tween_property(door_l, "global_position:x", global_position.x + open_width, open_time).as_relative()
+			doorTween.tween_property(door_r, "global_position:x", global_position.x - open_width, open_time).as_relative()
 			await doorTween.finished
 			open = false
 		else:
